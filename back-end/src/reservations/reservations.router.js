c669de03ac18fc4f9ef63bc71eb0e16670dd5d2f
EBassettHound <ebass227@gmail.com>
2023-11-17 12:43:47 -0500

    backend routes made, no functionality yet

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -8,5 +8,13 @@ const router = require("express").Router();
 const controller = require("./reservations.controller");
 
 router.route("/").get(controller.list);
+router.route("/:date")
+router.route("/new")
+router.route("/:reservation_id/seat")
+router.route("/:reservation_id/edit")
+router.route("/:reservation_id/status")
+
+
+
 
 module.exports = router;
